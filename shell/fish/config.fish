4c4380618eeb0965f5d5a6a9f5e1615370e8ce73
dczheng <dczheng21@outlook.com>
2024-06-27 15:51:17 +0800

    shell/fish/config.fish: Update

--- shell/fish/config.fish
+++ shell/fish/config.fish
@@ -2,6 +2,10 @@ alias vi="vim"
 alias df="df -h"
 alias dh="dh -h"
 alias l="ll"
+alias make="make -j$(cat /proc/cpuinfo | grep processor -c) "
+
+set -x OMP_NUM_THREADS 1
+set -x LC_ALL en_US.UTF-8
 
 #set -x GREP_COLOR "0;33"
 set -x PATH $ZCONFIG/tools $PATH
