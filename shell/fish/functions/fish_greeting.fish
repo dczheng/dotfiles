a78d1d1e9e269964894c2e6df2f741babc07a410
dczheng <dczheng21@outlook.com>
2025-11-25 04:45:33 +0800

    Update bash

--- shell/fish/functions/fish_greeting.fish
+++ shell/fish/functions/fish_greeting.fish
@@ -1,14 +1,6 @@
 # Defined in fish_greeting.fish @ line 1
 function fish_greeting
     if status is-login
-        clear 
-        set sys (cat /etc/hostname)
-        cowsay.sh "$USER (*.*) Welcome to $sys (*.*)"
-        #echo ">>>>>>>>>>>>>>>>>>>"
-        #echo ">>>>>>>>>>>>>>>>>>>"
-        #echo "Last Login:"
-        #lastlog | sed -n '3,$p' | egrep -v "(Never| tty)" | awk '{print $1" from "$3}'
-        #echo ">>>>>>>>>>>>>>>>>>>"
-        #date
+        cowsay.sh "Welcome to $(cat /etc/hostname)"
     end
 end
